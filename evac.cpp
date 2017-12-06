80feb03b1fc67f93d68ad01a3f2b11ec5b1a68cf
Sean C <seanallen.chen@gmail.com>
2017-12-05 23:38:49 -0800

    sean update
    deep copy and findCenter()

--- evac.cpp
+++ evac.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include "evac.h"
 #include "EvacRunner.h"
+#include <cmath>
 
 using namespace std;
 
@@ -9,7 +10,22 @@ Evac::Evac(City *citie, int numCitie, int numRoads) : numCities(numCitie)
   //cout << "numCitie = " << numCitie << endl;
   //cout << "numRoads = " << numRoads << endl;
   
+  //deep copy city TEMP
+  c = new City[numCities];//citie;
+  for(int x = 0; x < numCitie; x++)
+  {
+    c[x].ID = citie[x].ID;
+    c[x].x = citie[x].x;
+    c[x].y = citie[x].y;
+    c[x].population = citie[x].population;
+    c[x].evacuees = citie[x].evacuees;
+    c[x].roads = citie[x].roads;
+    c[x].roadCount = citie[x].roadCount;
+  }
+  
+  //init the adjacent list
   numAdjCities = new int[numCities];
+  // coordCities = new coordinates[numCities];
   adjList = new int*[numCities];
   
   for (int i = 0; i < numCities; i++)
@@ -17,16 +33,18 @@ Evac::Evac(City *citie, int numCitie, int numRoads) : numCities(numCitie)
     //cout << "city ID = " << citie[i].ID << endl;
     //cout << "roadCount = " << citie[i].roadCount << endl << endl;
     
-    numAdjCities[i] = citie[i].roadCount;
+    numAdjCities[i] = citie[i].roadCount;//roadcount is the number of roads connecting to adj cities
+    // coordCities[i].set(citie[i].x, citie[i].y);
     adjList[i] = new int[citie[i].roadCount + 1];
     
     //populate adjList
+    adjList[i][0] = citie[i].ID;
+    
     for (int j = 1; j <= numAdjCities[i]; j++)
     {
       adjList[i][j] = citie[i].roads[j - 1].destinationCityID;
     }
     
-    adjList[i][0] = citie[i].ID;
   }
   
   
@@ -58,6 +76,40 @@ Evac::Evac(City *citie, int numCitie, int numRoads) : numCities(numCitie)
 void Evac::evacuate(int *evacIDs, int numEvacs, EvacRoute *evacRoutes,
     int &routeCount)
 {
+  int rootID = findCenter(evacIDs, numEvacs);
+  cout << "root is: " << rootID << endl;
   //depth first search to determine which path to take (network flow)
 } // evacuate
 
+
+int Evac :: findCenter(int * evacCitiesID, int numEvacs)
+{
+  //find the center coordinates
+  double x = 0.0;
+  double y = 0.0;
+  for(int i = 0; i < numEvacs; i++)
+  {
+    x += c[evacCitiesID[i]].x;
+    y += c[evacCitiesID[i]].y;
+  }
+  x = x/(double)numEvacs;
+  y = y/(double)numEvacs;
+  
+  cout << "found centroid" << endl;
+  //find the closest adjacentID
+  int d = 0;
+  int min = 9999;
+  int the_chosen_one = 0; 
+  for(int i = 0; i < numEvacs; i++)
+  {
+    //distance between center points and coordinates of evacCitiesID
+    d = pow( (x - c[evacCitiesID[i]].x), 2 ) - pow( (y - c[evacCitiesID[i]].y), 2);
+    d = sqrt(d);
+    if(d <= min)
+    {
+      min = d;
+      the_chosen_one = i;
+    }
+  }
+  return the_chosen_one;//index closest to centroid
+}
--- evac.h
+++ evac.h
@@ -4,17 +4,27 @@
 
 #include "EvacRunner.h"
 
+// class coordinates
+// {
+  // int x;
+  // int y;
+  // public:
+    // void set(int xp, int yp){x = xp; y = yp;}
+// };
 
 class Evac
 {
   int numCities;
   int* numAdjCities;
+  City* c;
+  // coordinates* coordCities;
   int** adjList;
   
 public:
   Evac(City *cities, int numCities, int numRoads);
   void evacuate(int *evacIDs, int numEvacs, EvacRoute *evacRoutes,
     int &routeCount); // student sets evacRoutes and routeCount
+  int findCenter(int * evacCitiesID, int numEvacs);
   
 }; // class Evac
 
