a536ba1d6bf07113712038cd7431936dbb17c276
VivZing <32667327+VivZing@users.noreply.github.com>
2017-12-07 18:03:11 -0800

    Update evac.h

--- evac.h
+++ evac.h
@@ -3,14 +3,37 @@
 #define evacH
 
 #include "EvacRunner.h"
+#include <iostream>  //for NULL
+
+using namespace std;
+
+class Cities
+{
+public:
+  int ID;
+  int x;
+  int y;
+  int population;
+  int evacuees;
+  Road *roads;
+  int roadCount;
+  
+  bool evacCity;
+  int depth;
+
+
+  Cities() : evacuees(0), roads(NULL), roadCount(0){}
+  ~Cities(){delete [] roads;}
+};  // class Cities
 
 
 class Evac
 {
   int numCities;
-  City* city;
-  bool* evacCity;
-  int* cityDepth;
+  Cities* city;
+  //City* city;
+  //bool* evacCity;
+  //int* cityDepth;
   
   int** adjList;
   
